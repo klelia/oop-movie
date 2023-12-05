9bdbc695f54ada2bb1dcd5404b6fc1c1c9b7e488
klelia <clelia.fradella@gmail.com>
2023-12-05 15:42:03 +0100

    fix card and add book class

--- books.php
+++ books.php
@@ -1,8 +1,16 @@
 <?php
 include __DIR__ . '/Views/header.php';
+include __DIR__ . '/Model/Book.php';
+$books = Book::fetchAll();
 ?>
 <section class="container">
     <h2>Book</h2>
+    <div class="row gy-4">
+        <?php foreach ($books as $book) {
+            $book->printCard();
+        }
+        ?>
+    </div>
 </section>
 <?php
 include __DIR__ . '/Views/footer.php';
